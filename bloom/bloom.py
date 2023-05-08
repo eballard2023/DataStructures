30d0c065c044994dc7f68bfa7605f9916a1a8adf
Elijah Ballard <eballar1@terpmail.umd.edu>
2023-05-08 16:09:18 -0400

    done!

--- bloom/bloom.py
+++ bloom/bloom.py
@@ -1,11 +1,32 @@
 from __future__ import annotations
 import json
 from typing import List
+import math
 
-def hash(username:str,j:int,m:int) -> int:
-    # Adjust accordingly
-    result = 0
-    return(result)
+def hash(username: str, j: int, m: int) -> int:
+    ascii_convert = [ord(c) for c in username]
+    
+
+    ascii_add = sum(ascii_convert)
+
+    concat = ascii_add
+    while concat < m:
+        concat = int(str(concat) + str(concat))
+
+    jth = concat ** j
+
+    result = int(str(jth)[:len(str(m))])
+
+    result = result % m
+
+    return result
+
+
+
+
+
+
+
 
 # Bloom Filter Class
 # DO NOT MODIFY
@@ -14,7 +35,7 @@ class Bloom():
     def __init__(self,
                  m         = int,
                  k         = int,
-                 fpmax     = float,
+                 fpmax  = int,
                  threshold = int,
                  bitarray  = List[int],
                  usernamedict   = dict,
@@ -36,16 +57,97 @@ class Bloom():
     # If a username has been hacked, record it.
     # If it's hacked threshold times, insert it into the bloom filter.
     def hack(self,username: str):
-        # The dummy line is just to make it run.
-        dummy = 0
+       if username in self.usernamedict:
+           
+           self.usernamedict[username] += 1
+       else:
+           
+           self.usernamedict[username] = 1
+           
+     
+       if self.usernamedict[username] == self.threshold:
+         
+         self.insert(username)
 
     # Insert a username into the bloom filter.
-    def insert(self,username: str):
-        # The dummy line is just to make it run.
-        dummy = 0
+    def insert(self, username: str):
+
+        self.n = self.n + 1
+        for x in range(1, self.k + 1):
+            index = hash(username, x, self.m)
+            self.bitarray[index] = 1
+        
+        
+        false_pos = self.false_positive()
+       
+
+        if false_pos > self.fpmax:
+            self.rebuild()
+            
+       
+
+
+    def false_positive(self):
+        prob = (1-(1-(1/self.m))**(self.k * self.n))**self.k
+        return prob
+
 
     # Check if a username is in the bloom filter.
+    # Check if a username is in the bloom filter.
     def check(self,username:str) -> str:
-        # Adjust accordingly
-        conclusion = 'figure this out'
-        return(json.dumps({'username':username,'status':conclusion}))
+        for x in range(1, self.k + 1):
+            index = hash(username,x,self.m)
+            
+            if self.bitarray[index] == 0:
+                return json.dumps({'username': username, 'status': 'SAFE'})
+
+        return json.dumps({'username': username, 'status': 'UNSAFE'})
+
+    def rebuild(self):
+            
+            while self.false_positive() > self.fpmax / 2:
+                self.m += 1
+                
+                # self.k = max(1, int((self.m / self.n) * math.log(2)))
+
+            self.bitarray = [0] * self.m
+            print(self.bitarray)
+            
+            for username in self.usernamedict:
+                print(self.usernamedict)
+                if self.usernamedict[username] >= self.threshold:
+                    for x in range(1, self.k + 1):
+                        index = hash(username, x, self.m)
+                        self.bitarray[index] = 1
+        
+
+        
+        
+                
+
+
+        
+            
+           
+
+            
+if __name__ == '__main__':
+    bloom_filter = Bloom(7,4,.2,2)
+    bloom_filter.hack("Mikael")
+    bloom_filter.hack("Jedd")
+    bloom_filter.hack("Mikael")
+    bloom_filter.hack("Mikael")
+    bloom_filter.hack("Mikael")
+    bloom_filter.hack("Mikael")
+    bloom_filter.hack("Mikael")
+    bloom_filter.hack("Peter")
+    bloom_filter.hack("Jedd")
+    bloom_filter.hack("Jedd")
+    bloom_filter.hack("Jedd")
+   
+    
+    # 01111
+
+    print(bloom_filter.dump())
+
+    
